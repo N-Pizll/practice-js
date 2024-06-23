383e8150a1ce9a1cb65081fd240cc42f380f35c1
Nima Khastar <nimapizll.nn@gmail.com>
2024-06-24 01:08:59 +0330

    feat ; year
    year miladi

---
+++ Insurance/Insurance/js/app.js
@@ -0,0 +1,27 @@
+const year = document.getElementById("years");
+const level = document.getElementById("");
+const radio = document.getElementById("");
+
+// =================================================
+
+// function calcYears() {}
+// let nowyears = new Date().toLocaleString("fa-IR");
+// nowyears = nowyears.slice(0, 4);
+// // console.log(nowyears);
+
+// let max = nowyears;
+
+// let min = max - 20;
+// console.log(max);
+// console.log(min);
+
+function displayYear() {}
+const calyear = new Date().getFullYear();
+// y = calyear.slice(0, 4);
+console.log(calyear);
+let maxyear = calyear;
+
+let minyear = maxyear - 20;
+console.log(minyear);
+
+
