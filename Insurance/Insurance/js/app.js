a1d96b0ce5b4fb8a5d9bbd5511425c25c909bc5c
Nima Khastar <nimapizll.nn@gmail.com>
2024-06-24 02:02:17 +0330

    feat : js

--- Insurance/Insurance/js/app.js
+++ Insurance/Insurance/js/app.js
@@ -1,27 +1,29 @@
-const year = document.getElementById("years");
-const level = document.getElementById("");
-const radio = document.getElementById("");
+// Selected box and level ,radio
+const year = document.getElementById("#year");
+const level = document.getElementById("#LEVEL");
+const radio = document.getElementById("#RADIO");
 
+year.addEventListener('DOMContentLoaded',getYear)
 // =================================================
+//  function sall  shamsi
+function getYear() {
+    let hereyear = new Date().toLocaleDateString('fa-ir')
+    hereyear = hereyear.slice(0,4)
+    let maxYear = getYear();
+    
+    for (let i =maxYear; i >= maxYear - 20; i--){
+        const option = document.createElement('option');
+        option.value = i;
+        option.innerText=`سال${i}`
+    }
+    
+}
 
-// function calcYears() {}
-// let nowyears = new Date().toLocaleString("fa-IR");
-// nowyears = nowyears.slice(0, 4);
-// // console.log(nowyears);
 
-// let max = nowyears;
+console.log(getYear());
 
-// let min = max - 20;
-// console.log(max);
-// console.log(min);
 
-function displayYear() {}
-const calyear = new Date().getFullYear();
-// y = calyear.slice(0, 4);
-console.log(calyear);
-let maxyear = calyear;
 
-let minyear = maxyear - 20;
-console.log(minyear);
+
 
 
